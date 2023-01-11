ed858e3215854e3069bfd678741234abcd9add6b
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-01-11 21:58:42 +0900

    First init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,30 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+  <main>
+    <style jsx>{`
+          .index-wrapper {
+            width: 100vw;
+            height: 100vh;
+            background-color: white;
+            position: fixed;
+            top: 0;
+          }
+          .text-wrap {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            text-align: center;
+          }`}</style>
+    <div className="index-wrapper">
+      <div className="text-wrap">
+        <h1>Coloca</h1>
+        <p>Now developing!!</p>
+      </div>
+    </div>
+  </main>
+);
 
-export default IndexPage
+export default IndexPage;
